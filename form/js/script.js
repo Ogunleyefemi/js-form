e4d289fd639fc945fcc42df6208010ecfc861ab4
doszen <oluwafemiogunleye21@gmail.com>
2022-06-19 16:32:21 +0100

    add changes id person

--- form/js/script.js
+++ form/js/script.js
@@ -1,13 +1,34 @@
- $(document).ready(function() {
-  $("#formOne").submit(function() {
-    $(".person1").append("blah blah");
-    $(".person2").append("blah blah");
-    $(".animal").append("blah blah");
-    $(".exclamation").append("blah blah");
-    $(".verb").append("blah blah");
-    $(".noun").append("blah blah");
+//  $(document).ready(function() {
+//   $("#formOne").submit(function() {
+//     $(".person1").append("blah blah");
+//     $(".person2").append("blah blah");
+//     $(".animal").append("blah blah");
+//     $(".exclamation").append("blah blah");
+//     $(".verb").append("blah blah");
+//     $(".noun").append("blah blah");
 
-    $("#story").show();
-    event.preventDefault();
-  });
+//     $("#story").show();
+//     event.preventDefault();
+//   });
+// });
+$(document).ready(function() {
+    $("#formOne").submit(function(event) {
+      const person1Input = $("#person1").val();
+      const person2Input = $("#person2").val();
+      const animalInput= $("#animal").val();
+      const exclamationInput = $("#exclamation").val();
+      const verbInput = $("#verb").val();
+      const nounInput = $("#noun").val();
+  
+      $(".person1").append(person1Input);
+      $(".person2").append(person2Input);
+      $(".animal").append(animalInput);
+      $(".exclamation").append(exclamationInput);
+      $(".verb").append(verbInput);
+      $(".noun").append(nounInput);
+  
+      $("#story").show();
+  
+      event.preventDefault();
+    });
 });
