260dba667d362476471f0d91bf69d9615c274dc1
doszen <oluwafemiogunleye21@gmail.com>
2022-06-19 16:54:17 +0100

    add readme

---
+++ form/README.md
@@ -0,0 +1,5 @@
+## practicing form using the following stacks ##
+*HTML
+*CSS
+*JAVASCRIPT
+*jQUERY
--- form/js/script.js
+++ form/js/script.js
@@ -20,12 +20,12 @@ $(document).ready(function() {
       const verbInput = $("#verb").val();
       const nounInput = $("#noun").val();
   
-      $(".person1").append(person1Input);
-      $(".person2").append(person2Input);
-      $(".animal").append(animalInput);
-      $(".exclamation").append(exclamationInput);
-      $(".verb").append(verbInput);
-      $(".noun").append(nounInput);
+      $(".person1").text(person1Input);
+      $(".person2").text(person2Input);
+      $(".animal").text(animalInput);
+      $(".exclamation").text(exclamationInput);
+      $(".verb").text(verbInput);
+      $(".noun").text(nounInput);
   
       $("#story").show();
   
