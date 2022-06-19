0a365689d9001aa9a0e6ec89db2ee45d18a07839
doszen <oluwafemiogunleye21@gmail.com>
2022-06-19 14:41:53 +0100

    add local variable

---
+++ form/js/script.js
@@ -0,0 +1,9 @@
+function sampleFunction() {
+  let localString = "This is a local variable";
+  alert(localString);
+  localString = "This is a local variable update!!";
+  alert(localString);
+}
+
+sampleFunction();
+alert(localString);
